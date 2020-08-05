22b3310c8002acb06da9152db62fc1f55e358953
kira0415 <kirahuang0415@gmail.com>
2020-08-05 14:45:56 -0400

    added home swipe news

--- app/src/main/java/com/laioffer/tinnews/ui/home/HomeViewModel.java
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeViewModel.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
+import com.laioffer.tinnews.model.Article;
 import com.laioffer.tinnews.model.NewsResponse;
 import com.laioffer.tinnews.repository.NewsRepository;
 
@@ -25,5 +26,7 @@ public class HomeViewModel extends ViewModel {
         return Transformations.switchMap(countryInput, repository::getTopHeadlines);
     }
 
-
+    public void setFavoriteArticleInput(Article article) {
+        repository.favortieArticle(article);
+    }
 }
